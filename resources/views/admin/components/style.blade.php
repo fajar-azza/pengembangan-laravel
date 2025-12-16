de0e28fb75311fecc5e7a0644545f8f86ad95138
fajar-azza <fajarazzanajwa@gmail.com>
2025-12-16 15:51:03 +0700

    perubahan nama judul

--- resources/views/admin/components/style.blade.php
+++ resources/views/admin/components/style.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>Miscellaneous DataTables | CORK - Multipurpose Bootstrap Dashboard Template </title>
+    <title>Aplikasi KP | Aktif </title>
     <link rel="icon" type="image/x-icon" href="{{asset('')}}src/assets/img/favicon.ico"/>
     <link href="{{asset('layouts/semi-dark-menu/css/light/loader.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('layouts/semi-dark-menu/css/dark/loader.css')}}" rel="stylesheet" type="text/css" />
