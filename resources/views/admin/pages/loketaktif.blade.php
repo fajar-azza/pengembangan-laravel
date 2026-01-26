a50ca28e8ef21b1d001cad7bd7190887e649c50f
atta <atarik.shen05@gmail.com>
2026-01-26 21:01:32 +0700

    perubahan type password login

--- resources/views/admin/pages/loketaktif.blade.php
+++ resources/views/admin/pages/loketaktif.blade.php
@@ -1,124 +1,122 @@
 @extends('admin.layout.app')
 @section('content')
-
-
-
-<div class="row layout-spacing">
-    <div class="col-lg-12">
-        <div class="statbox widget box box-shadow">
-            <div class="widget-content widget-content-area">
-                <div id="style-2_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer">
-                    <div class="dt--top-section">
-                        <div class="row">
-                            <div class="col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center">
-                                <div class="dataTables_length" id="style-2_length"><label>Results : <select
-                                            name="style-2_length" aria-controls="style-2" class="form-control">
-                                            <option value="5">5</option>
-                                            <option value="10">10</option>
-                                            <option value="20">20</option>
-                                            <option value="50">50</option>
-                                        </select></label></div>
-                            </div>
-                            <div
-                                class="col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center mt-sm-0 mt-3">
-                                <div id="style-2_filter" class="dataTables_filter"><label><svg
-                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-search">
-                                            <circle cx="11" cy="11" r="8"></circle>
-                                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                                        </svg><input type="search" class="form-control" placeholder="Search..."
-                                            aria-controls="style-2"></label></div>
+    <div class="row layout-spacing">
+        <div class="col-lg-12">
+            <div class="statbox widget box box-shadow">
+                <div class="widget-content widget-content-area">
+                    <div id="style-2_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer">
+                        <div class="dt--top-section">
+                            <div class="row">
+                                <div class="col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center">
+                                    <div class="dataTables_length" id="style-2_length"><label>Results : <select
+                                                name="style-2_length" aria-controls="style-2" class="form-control">
+                                                <option value="5">5</option>
+                                                <option value="10">10</option>
+                                                <option value="20">20</option>
+                                                <option value="50">50</option>
+                                            </select></label></div>
+                                </div>
+                                <div
+                                    class="col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center mt-sm-0 mt-3">
+                                    <div id="style-2_filter" class="dataTables_filter"><label><svg
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-search">
+                                                <circle cx="11" cy="11" r="8"></circle>
+                                                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                                            </svg><input type="search" class="form-control" placeholder="Search..."
+                                                aria-controls="style-2"></label></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="table-responsive">
-                        <table id="style-2" class="table style-2 dt-table-hover dataTable no-footer" role="grid"
-                            aria-describedby="style-2_info" style="width: 1222px;">
-                            <thead>
-                                <tr role="row">
-                                    <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
-                                        colspan="1" aria-label="First Name: activate to sort column ascending"
-                                        style="width: 107px;">No</th>
-                                    <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
-                                        colspan="1" aria-label="First Name: activate to sort column ascending"
-                                        style="width: 107px;">First Name</th>
-                                    <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
-                                        colspan="1" aria-label="Last Name: activate to sort column ascending"
-                                        style="width: 104px;">Last Name</th>
-                                    <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
-                                        colspan="1" aria-label="Email: activate to sort column ascending"
-                                        style="width: 177px;">Email</th>
-                                    <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
-                                        colspan="1" aria-label="Mobile No.: activate to sort column ascending"
-                                        style="width: 130px;">Mobile No.</th>
-                                    <th class="text-center sorting" tabindex="0" aria-controls="style-2"
-                                        rowspan="1" colspan="1"
-                                        aria-label="Image: activate to sort column ascending" style="width: 64px;">Image
-                                    </th>
-                                    <th class="text-center sorting" tabindex="0" aria-controls="style-2"
-                                        rowspan="1" colspan="1"
-                                        aria-label="Status: activate to sort column ascending" style="width: 103px;">
-                                        Status</th>
-                                    <th class="text-center dt-no-sorting sorting" tabindex="0"
-                                        aria-controls="style-2" rowspan="1" colspan="1"
-                                        aria-label="Action: activate to sort column ascending" style="width: 68px;">
-                                        Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                        <div class="table-responsive">
+                            <table id="style-2" class="table style-2 dt-table-hover dataTable no-footer" role="grid"
+                                aria-describedby="style-2_info" style="width: 1222px;">
+                                <thead>
+                                    <tr role="row">
+                                        <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
+                                            colspan="1" aria-label="First Name: activate to sort column ascending"
+                                            style="width: 107px;">No</th>
+                                        <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
+                                            colspan="1" aria-label="First Name: activate to sort column ascending"
+                                            style="width: 107px;">First Name</th>
+                                        <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
+                                            colspan="1" aria-label="Last Name: activate to sort column ascending"
+                                            style="width: 104px;">Last Name</th>
+                                        <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
+                                            colspan="1" aria-label="Email: activate to sort column ascending"
+                                            style="width: 177px;">Email</th>
+                                        <th class="sorting" tabindex="0" aria-controls="style-2" rowspan="1"
+                                            colspan="1" aria-label="Mobile No.: activate to sort column ascending"
+                                            style="width: 130px;">Mobile No.</th>
+                                        <th class="text-center sorting" tabindex="0" aria-controls="style-2"
+                                            rowspan="1" colspan="1"
+                                            aria-label="Image: activate to sort column ascending" style="width: 64px;">Image
+                                        </th>
+                                        <th class="text-center sorting" tabindex="0" aria-controls="style-2"
+                                            rowspan="1" colspan="1"
+                                            aria-label="Status: activate to sort column ascending" style="width: 103px;">
+                                            Status</th>
+                                        <th class="text-center dt-no-sorting sorting" tabindex="0" aria-controls="style-2"
+                                            rowspan="1" colspan="1"
+                                            aria-label="Action: activate to sort column ascending" style="width: 68px;">
+                                            Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-                                <tr role="row" class="odd">
-                                    <td>1</td>
-                                    <td>Jane</td>
-                                    <td>Lamb</td>
-                                    <td>johndoe@yahoo.com</td>
-                                    <td>555-555-5555</td>
-                                    <td class="text-center">
-                                        <span><img src="../src/assets/img/profile-9.jpeg"
-                                                class="rounded-circle profile-img" alt="avatar"></span>
-                                    </td>
-                                    <td class="text-center"><span
-                                            class="shadow-none badge badge-primary">Approved</span></td>
-                                    
-                                </tr>
-                                
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="dt--bottom-section d-sm-flex justify-content-sm-between text-center">
-                        <div class="dt--pages-count  mb-sm-0 mb-3">
-                            <div class="dataTables_info" id="style-2_info" role="status" aria-live="polite">Showing
-                                page 1 of 1</div>
+                                    <tr role="row" class="odd">
+                                        <td>1</td>
+                                        <td>Jane</td>
+                                        <td>Lamb</td>
+                                        <td>johndoe@yahoo.com</td>
+                                        <td>555-555-5555</td>
+                                        <td class="text-center">
+                                            <span><img src="../src/assets/img/profile-9.jpeg"
+                                                    class="rounded-circle profile-img" alt="avatar"></span>
+                                        </td>
+                                        <td class="text-center"><span
+                                                class="shadow-none badge badge-primary">Approved</span></td>
+
+                                    </tr>
+
+                                </tbody>
+                            </table>
                         </div>
-                        <div class="dt--pagination">
-                            <div class="dataTables_paginate paging_simple_numbers" id="style-2_paginate">
-                                <ul class="pagination">
-                                    <li class="paginate_button page-item previous disabled" id="style-2_previous"><a
-                                            href="#" aria-controls="style-2" data-dt-idx="0" tabindex="0"
-                                            class="page-link"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                height="24" viewBox="0 0 24 24" fill="none"
-                                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                stroke-linejoin="round" class="feather feather-arrow-left">
-                                                <line x1="19" y1="12" x2="5" y2="12">
-                                                </line>
-                                                <polyline points="12 19 5 12 12 5"></polyline>
-                                            </svg></a></li>
-                                    <li class="paginate_button page-item active"><a href="#"
-                                            aria-controls="style-2" data-dt-idx="1" tabindex="0"
-                                            class="page-link">1</a></li>
-                                    <li class="paginate_button page-item next disabled" id="style-2_next"><a
-                                            href="#" aria-controls="style-2" data-dt-idx="2" tabindex="0"
-                                            class="page-link"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                height="24" viewBox="0 0 24 24" fill="none"
-                                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                stroke-linejoin="round" class="feather feather-arrow-right">
-                                                <line x1="5" y1="12" x2="19" y2="12">
-                                                </line>
-                                                <polyline points="12 5 19 12 12 19"></polyline>
-                                            </svg></a></li>
-                                </ul>
+                        <div class="dt--bottom-section d-sm-flex justify-content-sm-between text-center">
+                            <div class="dt--pages-count  mb-sm-0 mb-3">
+                                <div class="dataTables_info" id="style-2_info" role="status" aria-live="polite">Showing
+                                    page 1 of 1</div>
+                            </div>
+                            <div class="dt--pagination">
+                                <div class="dataTables_paginate paging_simple_numbers" id="style-2_paginate">
+                                    <ul class="pagination">
+                                        <li class="paginate_button page-item previous disabled" id="style-2_previous"><a
+                                                href="#" aria-controls="style-2" data-dt-idx="0" tabindex="0"
+                                                class="page-link"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                    height="24" viewBox="0 0 24 24" fill="none"
+                                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                    stroke-linejoin="round" class="feather feather-arrow-left">
+                                                    <line x1="19" y1="12" x2="5" y2="12">
+                                                    </line>
+                                                    <polyline points="12 19 5 12 12 5"></polyline>
+                                                </svg></a></li>
+                                        <li class="paginate_button page-item active"><a href="#"
+                                                aria-controls="style-2" data-dt-idx="1" tabindex="0"
+                                                class="page-link">1</a></li>
+                                        <li class="paginate_button page-item next disabled" id="style-2_next"><a
+                                                href="#" aria-controls="style-2" data-dt-idx="2" tabindex="0"
+                                                class="page-link"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                    height="24" viewBox="0 0 24 24" fill="none"
+                                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                    stroke-linejoin="round" class="feather feather-arrow-right">
+                                                    <line x1="5" y1="12" x2="19" y2="12">
+                                                    </line>
+                                                    <polyline points="12 5 19 12 12 19"></polyline>
+                                                </svg></a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -126,5 +124,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
